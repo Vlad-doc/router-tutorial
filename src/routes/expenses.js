375d7eeef16a4bbfcff5cd6dbf9503e2614000cd
Your Name <you@example.com>
2022-06-06 13:42:12 +0300

    create basic routes

---
+++ src/routes/expenses.js
@@ -0,0 +1,7 @@
+export default function Expenses() {
+  return (
+    <main>
+      <h2 style={{ padding: "1 rem 0" }}>Expenses</h2>
+    </main>
+  )
+}
---
+++ src/routes/invoices.js
@@ -0,0 +1,7 @@
+export default function Invoices() {
+  return (
+    <main>
+      <h2 style={{ padding: "1 rem 0" }}>Invoices</h2>
+    </main>
+  )
+}
