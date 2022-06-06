2465200f993f62faf8de29e74d5e37ee65069816
Your Name <you@example.com>
2022-06-06 15:28:27 +0300

    add search input for invoice

--- src/routes/invoices.js
+++ src/routes/invoices.js
@@ -1,20 +1,49 @@
-import { Link, Outlet } from "react-router-dom"
+import { NavLink, Outlet, useSearchParams } from "react-router-dom"
 import { getInvoices } from "../data"
 
 export default function Invoices() {
   let invoices = getInvoices()
+  const [searchParams, setSearchParams] = useSearchParams()
+  console.log(searchParams)
+
   return (
     <div style={{ display: "flex" }}>
       <nav style={{ borderRight: "solid, 1px", padding: "1rem" }}>
-        {invoices.map((invoice) => (
-          <Link
-            key={invoice.number}
-            to={`/invoices/${invoice.number}`}
-            style={{ display: "block", margin: "1rem 0" }}
-          >
-            {invoice.name}
-          </Link>
-        ))}
+        <input
+          type="text"
+          value={searchParams.get("filter") || ""}
+          onChange={(event) => {
+            let filter = event.target.value
+            if (filter) {
+              setSearchParams({ filter })
+            } else {
+              setSearchParams({})
+            }
+          }}
+        />
+        {invoices
+          .filter((invoice) => {
+            let filter = searchParams.get("filter")
+            if (!filter) return true
+            let name = invoice.name.toLocaleLowerCase()
+            return name.startsWith(filter.toLocaleLowerCase())
+          })
+
+          .map((invoice) => (
+            <NavLink
+              key={invoice.number}
+              to={`/invoices/${invoice.number}`}
+              style={({ isActive }) => {
+                return {
+                  display: "block",
+                  margin: "1rem 0",
+                  color: isActive ? "red" : "",
+                }
+              }}
+            >
+              {invoice.name}
+            </NavLink>
+          ))}
       </nav>
       <Outlet />
     </div>
