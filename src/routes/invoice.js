18e7135ba601689836c14c819b95f7b6b1b308a7
Your Name <you@example.com>
2022-06-06 15:46:18 +0300

    delete invoice path

--- src/routes/invoice.js
+++ src/routes/invoice.js
@@ -1,9 +1,11 @@
-import { useParams } from "react-router-dom"
-import getInvoice from "../data"
+import { useLocation, useNavigate, useParams } from "react-router-dom"
+import getInvoice, { deleteInvoice } from "../data"
 
 export default function Invoice() {
   let params = useParams()
   let invoice = getInvoice(parseInt(params.invoiceId, 10))
+  let navigate = useNavigate()
+  let location = useLocation()
   return (
     <main>
       <h2>Total due: {invoice.amount}</h2>
@@ -11,6 +13,16 @@ export default function Invoice() {
         {invoice.name}: {invoice.number}
       </p>
       <p>Due Date: {invoice.due}</p>
+      <p>
+        <button
+          onClick={() => {
+            deleteInvoice(invoice.number)
+            navigate("invoices" + location.search)
+          }}
+        >
+          Delete
+        </button>
+      </p>
     </main>
   )
 }
